c6cc4b9f11010ffa829343cc08aae20654074f6b
Mary-Anne Triggs <maryannetriggs@Mary-Annes-MacBook-Pro.local>
2019-08-06 12:23:26 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
+// array to store our four cards
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardFour);
-console.log("User flipped " + cardOne);
+//empty array for cards in play
+var cardsInPlay = [];
+
+// creating variables for both cards in play
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+// adding card one to cards in play array
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+//test
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardTwo);
+
+//checking two cards have been selected by user
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("you've found a match!");
+	} else {
+		alert("Sorry, try again");
+	};
+} else {
+	//action
+};
+
+//logic to check if cards match
