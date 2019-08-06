2a2e1e2cfc3c291fbcb2c6c09524d1c4c5c86789
Mary-Anne Triggs <maryannetriggs@Mary-Annes-MacBook-Pro.local>
2019-08-06 15:39:55 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,30 +1,38 @@
-// array to store our four cards
+// array to store the four cards
 var cards = ["queen", "queen", "king", "king"];
 
 //empty array for cards in play
 var cardsInPlay = [];
 
-// creating variables for both cards in play
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-// adding card one to cards in play array
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-//test
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardTwo);
-
-//checking two cards have been selected by user
-if (cardsInPlay.length === 2) {
+//function to check for m{atch
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("you've found a match!");
+  console.log("You found a match!");
 	} else {
-		alert("Sorry, try again");
+  console.log("Sorry, try again.");
 	};
-} else {
-	//action
 };
 
-//logic to check if cards match
+//function for when user flips cards
+function flipCard(cardId) {
+
+	//displaying which card has been flipped
+	console.log("user flipped " + cards[cardId]);
+
+	//pushing selected card in empty array
+	cardsInPlay.push(cards[cardId]);
+
+	//comparing two cards selected by user
+	if (cardsInPlay.length === 2) {
+		checkForMatch() === true;
+	} else {
+		//potential action here
+	};
+	
+};
+
+
+flipCard(0);
+flipCard(2);
+
+
